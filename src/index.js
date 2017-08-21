e3cdad2d7f43453519cefa9c81ada95b500c1b76
HaiXuan123 <2269287394@qq.com>
2017-08-21 09:58:05 +0800

    8.21

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {Provider} from 'react-redux'
-import {createStore,applyMiddleware} from 'redux'
-import reducer from './reducer/reducer'
-import thunk from 'redux-thunk'
+import {Provider} from 'react-redux';
+import {createStore,applyMiddleware} from 'redux';
+import reducer from './reducer/reducer';
+import thunk from 'redux-thunk';
+import './rem'
 
 let store=createStore(reducer,applyMiddleware(thunk))
 ReactDOM.render(
