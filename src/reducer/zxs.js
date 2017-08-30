c2c685721dc137c25c54556900dc40fdbe64f25a
LotheringDD <1262795847@qq.com>
2017-08-30 14:08:46 +0800

    所有邮包

--- src/reducer/zxs.js
+++ src/reducer/zxs.js
@@ -1,6 +1,8 @@
 /**
  * Created by Administrator on 2017/8/20.
  */
+
+//物业通知
 const newTongzhi = (state=[],action) => {
     switch (action.type) {
         case 'TONGZHI':
@@ -10,4 +12,14 @@ const newTongzhi = (state=[],action) => {
 
     }
 };
-export { newTongzhi }
+//所有邮包
+const newParcel = (state=[],action) => {
+    switch (action.type) {
+        case 'YOUBAO':
+            return action.data;
+        default:
+            return state;
+
+    }
+};
+export { newTongzhi,newParcel }
